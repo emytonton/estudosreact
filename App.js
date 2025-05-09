25d87a3de574a37e14af1ae591229938f96ed1ce
Emilly Paiva <emillypaiva3260@gmail.com>
2025-05-09 16:28:14 -0300

    instagram começado

--- App.js
+++ App.js
@@ -1,102 +1,96 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, ScrollView, FlatList, Switch, TextInput, Button, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, FlatList, Switch, TextInput, Button, TouchableOpacity, Image} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import Slider from "@react-native-community/slider";
+import Lista from "./src/Lista";
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      nome: '',
-      idade: '',
-      genero: 0,  // armazena o valor selecionado no picker 
-      opcoesGerero: [
-        { key: 1, nome: 'Feminino' },
-        { key: 2, nome: 'Masculino' },
-        { key: 3, nome: 'Outros' },
-      ],
-      valor: 0,
-      estudante: false,
+      feed: [
+        {
+          id: '1', 
+          nome: 'Lucas Silva', 
+          descricao: 'Mais um dia de muitos bugs :)', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
+          likeada: false, 
+          likers: 0
+         },
+        {
+          id: '2', 
+          nome: 'Matheus', 
+          descricao: 'Isso sim é ser raiz!!!!!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
+          likeada: false, 
+          likers: 0
+        },
+        {
+          id: '3', 
+          nome: 'Jose Augusto', 
+          descricao: 'Bora trabalhar Haha', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
+          likeada: false, 
+          likers: 3
+        },
+        {
+          id: '4', 
+          nome: 'Gustavo Henrique', 
+          descricao: 'Isso sim que é TI!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
+          likeada: false, 
+          likers: 1
+        },
+        {
+          id: '5', 
+          nome: 'Guilherme', 
+          descricao: 'Boa tarde galera do insta...', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+          likeada: false, 
+          likers: 32
+        }
+      ]
+      
     };
   };
 
   render() {
     return (
+
       <View style={styles.container}>
 
-        
-        <Text style={styles.logo}>
-          Banco center
-        </Text>
 
-        <Text style= {styles.subTitulo}>A um cadastro de solucionar seus problemas!</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite o seu nome completo:"
-          onChangeText={(textoDigitado) => this.setState({ nome: textoDigitado })} // atualiza o estado com o texto digitado
-          value={this.state.nome}// define o valor atual do input com o estado
-        />
-
-        <TextInput
-          style={styles.input}
-          placeholder="Digite sua idade:"
-          onChangeText={(textoDigitadoIdade) => this.setState({ idade: textoDigitadoIdade })}
-          value={this.state.idade}
-        />
-
-      <Text style={styles.text}>Qual o seu genero?</Text>
-        <Picker
-          selectedValue={this.state.genero}
-          onValueChange={(itemValue, itemIndex) => this.setState({ genero: itemValue })}
-        >
-          {/* percorre o array de opções e gera um item do picker para cada uma */}
-          {this.state.opcoesGerero.map((v) => (
-            <Picker.Item key={v.key} value={v.key} label={v.nome} />
-          ))}
-        </Picker>
-        <Text style = {styles.text}>Escolha seu limite:</Text>
-
-        <Slider
-        minimumValue={0}
-        maximumValue={10000}
-        onValueChange={(valorSelecionado) => this.setState({valor:valorSelecionado})}
-        value={this.state.valor}
-        minimumTrackTintColor="purple"
-        maximumTrackTintColor="purple"
-        thumbTintColor="purple"
-        />
+        <View style={styles.header}>
+          <TouchableOpacity>
+          <Image
+          source = {require('./src/img/logo.png')}
+          style = {styles.logo}
+          />
+          </TouchableOpacity>
 
 
-        <Text style = {styles.textslider}>{this.state.valor.toFixed(0)}</Text>
-
-
-        <Text style = {styles.text}>Você é estudante?</Text>
-
-        <View style = {styles.switchContainer}>
-        <Switch 
-        onValueChange={(acionado) => this.setState({ estudante: acionado })}
-        value={this.state.estudante}
-        trackColor={{ false: '#ccc', true: 'purple' }} 
-        thumbColor={this.state.estudante ? '#fff' : '#888'} 
-        />
+          <TouchableOpacity>
+          <Image
+          source = {require('./src/img/send.png')}
+          style = {styles.send}
+          />
+          </TouchableOpacity>
         </View>
-          
-        <TouchableOpacity 
-        style={styles.button} 
-        onPress={() => alert(
-        'Seus dados inseridos:\n' +
-        'Nome: ' + this.state.nome + '\n' +
-        'Idade: ' + this.state.idade + '\n' +
-        'Gênero: ' + this.state.opcoesGerero[this.state.genero].nome + '\n' +
-        'Seu limite selecionado: ' + this.state.valor.toFixed(0) + '\n' +
-        'Estudante: ' + (this.state.estudante ? 'Sim' : 'Não')
-  )}
->
 
-          <Text style={styles.buttonText}>Criar sua conta</Text>
-        </TouchableOpacity>
-
+        <FlatList 
+        showsHorizontalScrollIndicator = {false}
+        keyExtractor={(item) => item.id }
+        data = {this.state.feed}
+        renderItem={ ({item}) => <Lista data={item} />}
+        />
+        
+        
       </View>
     );
   }
@@ -105,67 +99,26 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
     
   },
-  input: {
-    height: 40,
-    borderColor: 'purple',
-    borderWidth: 1,
-    paddingHorizontal: 10,
-    margin: 10,
-    marginBottom: 20,
-    borderRadius: 5,
-    
+  header:{
+    height:55,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 6,
+
+    borderBottomWidth:0.2,
+    shadowColor: '#000',
+    elevation: 1,
   },
-  logo: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: 'purple',
-    textAlign: 'center',
-    marginBottom: 20,
-    textTransform: 'uppercase',
-    letterSpacing: 1.5,
-  },
-  text: {
-    fontSize: 18,
-    marginLeft: 10,
-    marginTop: 6,
-    
-  },
-  textslider:{
-    fontSize: 18,
-    marginLeft: 10,
-    marginTop: 6,
-    textAlign: 'center'
+  logo:{
 
   },
-  button: {
-    borderRadius: 8,
-    margin: 10,
-    backgroundColor: 'purple',
-    padding: 12,
-    alignItems: 'center'
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 16
-  },
-  subTitulo:{
-    fontSize:18,
-    textAlign: 'center',
-    color: 'purple',
-    marginBottom: 15,
-  },
-  switchContainer: {
-    flexDirection: 'row',  // mantém switch à esquerda
-    alignItems: 'center',
-    margin: 15
-  },
-  switchLabel: {
-    marginLeft: 10,
-    fontSize: 16
+  send: {
+    width:23,
+    height:23
   }
   
 
