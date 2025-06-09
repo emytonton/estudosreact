05ba5e5caf782c72dcc63989c528cc420d7bead5
Emilly Paiva <emillypaiva3260@gmail.com>
2025-06-09 09:37:00 -0300

    hook useMemo

--- src/AprendendoHooks/Hooks-useMemo.js
+++ src/AprendendoHooks/Hooks-useMemo.js
@@ -1,19 +1,55 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useMemo} from "react";
 import { View, Text, StyleSheet, ScrollView, FlatList, Switch, TextInput, Button, TouchableOpacity, Image,Keyboard, Modal} from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
 
 export default function App(){
 
-  const [nome, setNome] = useState('Matheus');
+  const [nome, setNome] = useState('');
   const [input, setInput] = useState('');
+
+
+  //ComponentDidMout
+  useEffect( () => {
+    async function getStorage() {
+        const nomeStorage = await AsyncStorage.getItem('nomes');
+        if(nomeStorage !== null){
+          setNome(nomeStorage);
+        }
+    }
+
+    getStorage();
+  }, []);
+
+
+  //Simulando o nosso component DidUpdate
+  useEffect( () => {
+
+    async function saveStorage() {
+        await AsyncStorage.setItem('nomes', nome)
+    }
+
+
+    saveStorage();
+  }, [nome])
 
   function alteraNome(){
     setNome(input);
     setInput('');
   }
 
+
+  const letrasNome = useMemo( () => {
+    nome.length
+    console.log('mudou letra');
+    return nome.length;
+  }, [nome]);
+ 
+
   return(
     <View style = {styles.container}>
-/
+
       <TextInput placeholder="Seu nome .." value= {input} onChangeText={(text) => setInput(text)}> 
 
       </TextInput>
@@ -23,6 +59,9 @@ export default function App(){
         <Text style = {styles.textbnt}>Alterar nome</Text>
       </TouchableOpacity>
       <Text style = {styles.texto}>{nome}</Text>
+
+
+      <Text style = {styles.texto}>{letrasNome}</Text>
     </View>
   );
 }
